caeb628e59895f4b70e1ad4fe55918de51c08aea
Tyrone Vargas <77775585+tyroneley@users.noreply.github.com>
2026-04-17 10:39:33 +0700

    upd

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma?: PrismaClient
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
